eff007345e2742093222db80faa4a16f5fbaa9b7
nobreezagourmet <nobreezagourmet@gmail.com>
2026-02-02 02:41:29 -0300

    Sincronizacao final de rotas Front e Back

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -112,7 +112,7 @@ const createRaffle = asyncHandler(async (req, res) => {
         await session.commitTransaction();
         session.endSession();
 
-        return res.status(201).json({ success: true });
+        return res.status(201).json({ success: true, message: 'Rifa criada!' });
 
     } catch (error) {
         console.error(' ERRO AO CRIAR RIFA:', error);
--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ app.use((req, res, next) => {
 // CORS RADICAL - URL VERCEL CORRIGIDA
 app.use(require('cors')({ 
     origin: [
-        'https://ddevs.vercel.app', // Frontend na Vercel - URL CORRETA
+        'https://ddevss.vercel.app', // Frontend na Vercel - URL CORRETA
         'https://ddevs-86w2.onrender.com', // Backend (painel admin)
         'http://localhost:3000', 
         'http://localhost:5173'
